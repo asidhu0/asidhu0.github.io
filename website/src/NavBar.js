c84c86e30a9ddce6ce15345ae46e9a52526fdd4a
asidhu0 <asidhu359@gmail.com>
2024-06-30 00:35:41 -0700

    mvp

---
+++ website/src/NavBar.js
@@ -0,0 +1,19 @@
+// NavBar.js
+import React from 'react';
+import styles from "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar-container">
+      <nav className="navbar">
+        <a href="#home" className="nav-link">Home</a>
+        <a href="#experience" className="nav-link">Experience</a>
+        <a href="#projects" className="nav-link">Projects</a>
+        <a href="#contact" className="nav-link">Contact</a>
+      </nav>
+      <hr className="line" />
+    </div>
+  );
+}
+
+export default NavBar;
